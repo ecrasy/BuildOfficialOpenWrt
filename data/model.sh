1240f9fb23edeffa561bbd8389aa79034f544ab6
Carbon <ecrasy@gmail.com>
2022-10-24 09:24:00 +0800

    remove unused packages

--- data/model.sh
+++ data/model.sh
@@ -4,13 +4,18 @@
 # Description: replace default model string with empty string
 # will be called in wlan up event
 # Created Time: 2022-07-25 01:30:41 UTC
-# Modified Time: 2022-10-10 04:08:28 UTC
+# Modified Time: 2022-10-24 01:22:31 UTC
 #########################################################################
 
 
 #!/bin/bash
 
 str="Default string Default string/Default string - "
+rpl=""
+cmd="s@$str@$rpl@"
+sed -i "$cmd" /tmp/sysinfo/model
+
+str="Default string Default string"
 rpl=""
 cmd="s@$str@$rpl@"
 sed -i "$cmd" /tmp/sysinfo/model
