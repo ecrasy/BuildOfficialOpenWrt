76e409ffed4303fe69a1c0ad586afd32861a1a8d
carbon <ecrasy@gmail.com>
2026-08-18 16:58:56 +0800

    fix.sh:update dockerd fix code

--- fix.sh
+++ fix.sh
@@ -3,7 +3,7 @@
 # Author: Carbon (ecrasy@gmail.com)
 # Description: feel free to use
 # Created Time: 2022-07-30 04:57:44 UTC
-# Modified Time: 2026-08-18 08:41:14 UTC
+# Modified Time: 2026-08-18 08:56:14 UTC
 #########################################################################
 
 
@@ -243,7 +243,7 @@ dockerd_path="feeds/packages/utils/dockerd/patches/001-skip-copy-nested-binaries
 if [ -f ${dockerd_path} ]; then
     echo "dockerd patch already exists"
 else
-    mkdir -p feeds/packages/utils/dockerd/patches
+    mkdir -p $(dirname $dockerd_path)
     cp $GITHUB_WORKSPACE/data/patches/dockerd.patch $dockerd_path
     echo "Fix dockerd nested copy error"
 fi
